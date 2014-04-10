293868106b226830ad48a78ee317b1dfd13173e6
Cesar Augusto Garcia <cesar.garcia@mulesoft.com>
2014-04-10 11:22:24 -0300

    Adding UpdateApplicationMetadata method

--- cloudhubClient.py
+++ cloudhubClient.py
@@ -1,6 +1,7 @@
 import sys
 from modules import GetApplicationInfo
 from modules import GetAllApplications
+from modules import UpdateApplicationMetadata
 
 # Recieve from commandline parameters
 if sys.argv[1]=='getApplicationInfo':
@@ -19,3 +20,14 @@ if sys.argv[1]=='getAllAplications':
 		GetAllApplications.make_request(cloudhub_user, cloudhub_pass)
 	else:
 		print "Missing parameters. Should be like 'python cloudhubClient.py getAllAplications accountUser accountPassword'"
+
+
+if sys.argv[1]=='updateApplicationMetada':
+	if len(sys.argv)==6:
+		cloudhub_user = sys.argv[2]
+		cloudhub_pass =	sys.argv[3]
+		app_name = sys.argv[4]
+		properties_path = sys.argv[5]
+		UpdateApplicationMetadata.make_request(cloudhub_user, cloudhub_pass, app_name, properties_path)
+	else:
+		print "Missing parameters. Should be like 'python cloudhubClient.py updateApplicationMetada accountUser accountPassword appDomain propertiesPath'"
