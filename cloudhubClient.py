a3ecbc3452e4c26856d12c6938a0c2a4f942548a
Cesar Augusto Garcia <cesaraugustogarcia86@gmail.com>
2014-05-05 02:39:51 -0300

    Implementation of arguments parser

--- cloudhubClient.py
+++ cloudhubClient.py
@@ -1,13 +1,24 @@
 import sys
 import importlib
+import argparse
 
-methods = {"getApplicationInfo":"GetApplicationInfo", "getAllAplications":"GetAllApplications", "updateApplicationMetada":"UpdateApplicationMetadata", "deleteApplication":"DeleteApplication", "createApplication":"CreateApplication"}
+from modules import GetApplicationInfo
+from modules import GetAllApplications
 
 
-if sys.argv[1] in methods.keys():
-    module = importlib.import_module("." + methods.get(sys.argv[1]),"modules")
-    module.make_request(sys.argv)
-else:
-    print "Not a valid method. Try one of "
-    print methods.keys()
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers(title='subcommands',description='valid subcommands')
 
+getApplicationInfo_parser = subparsers.add_parser('gai', help='Get Application Info method')
+getApplicationInfo_parser.add_argument('-app_name',required=True)
+getApplicationInfo_parser.add_argument('-cloudhub_user',required=True)
+getApplicationInfo_parser.add_argument('-cloudhub_pass',required=True)
+getApplicationInfo_parser.set_defaults(func=GetApplicationInfo.make_request)
+
+getAllApplications_parser = subparsers.add_parser('gaa', help='Get All Applications method')
+getAllApplications_parser.add_argument('-cloudhub_user',required=True)
+getAllApplications_parser.add_argument('-cloudhub_pass',required=True)
+getAllApplications_parser.set_defaults(func=GetAllApplications.make_request)
+
+args = parser.parse_args()
+args.func(vars(args))
--- modules/GetAllApplications.py
+++ modules/GetAllApplications.py
@@ -5,28 +5,24 @@ import pprint
 
 # Documentation: http://www.mulesoft.org/documentation/display/current/Get+Application
 def make_request(arguments):
-    if len(arguments) == 4:
-        cloudhub_user = arguments[2]
-        cloudhub_pass = arguments[3]
-        
-        baseurl = 'https://cloudhub.io/api/applications/'
-
-        response = requests.get(baseurl, auth=(cloudhub_user, cloudhub_pass))
-
-        json_response = json.loads(response.text)
-
-        pp = pprint.PrettyPrinter()
-
-        def my_safe_repr(object, context, maxlevels, level):
-            typ = pprint._type(object)
-            if typ is unicode:
-                object = str(object)
-            return pprint._safe_repr(object, context, maxlevels, level)
-
-
-        pp.format = my_safe_repr
-
-        pp.pprint(json_response)
+    cloudhub_user = arguments.get('cloudhub_user')
+    cloudhub_pass = arguments.get('cloudhub_pass')
     
-    else:
-        print "Missing parameters. Should be like 'python cloudhubClient.py getAllAplications accountUser accountPassword'"
+    baseurl = 'https://cloudhub.io/api/applications/'
+
+    response = requests.get(baseurl, auth=(cloudhub_user, cloudhub_pass))
+
+    json_response = json.loads(response.text)
+
+    pp = pprint.PrettyPrinter()
+
+    def my_safe_repr(object, context, maxlevels, level):
+        typ = pprint._type(object)
+        if typ is unicode:
+            object = str(object)
+        return pprint._safe_repr(object, context, maxlevels, level)
+
+
+    pp.format = my_safe_repr
+
+    pp.pprint(json_response)
